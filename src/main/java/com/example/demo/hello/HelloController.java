f3d6c64c21fdc2ed093131c28023da299f43a5af
maetaku <maetaku@localhost>
2021-09-04 12:51:30 +0900

    課題4、DB接続

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.hello;
 //github レビューテスト
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 
 public class HelloController {
+	
+	@Autowired
+	private HelloService service;
 
 	@GetMapping("/hello")
 	public String getHello() {
@@ -25,4 +29,15 @@ public class HelloController {
 		model.addAttribute("sample", str);
 		return "hello/response";
 	}
+	
+	@PostMapping("/hello/db")
+	public String postDbRequest(@RequestParam("text2")String id, Model model) {
+	//1件検索
+	Employee employee = service.getEmployee(id);
+	//検索結果をModelに登録
+	model.addAttribute("employee", employee);
+	//db.htmlに画面遷移
+	return "hello/db";
+	}
+	
 }
