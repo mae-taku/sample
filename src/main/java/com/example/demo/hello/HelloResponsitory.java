874640fd54b6a1e353ec119a3636cec0d50d5b7e
maetaku <maetaku@localhost>
2021-09-07 12:55:44 +0900

    contorollerの@controllerとクラスの間の改行を消した。Responsitoryのselect文修正(各接続に空白抜けてた？)。

--- src/main/java/com/example/demo/hello/HelloResponsitory.java
+++ src/main/java/com/example/demo/hello/HelloResponsitory.java
@@ -15,12 +15,12 @@ public class HelloResponsitory {
 	public Map<String, Object>findByld(String id){
 		
 		//SELECT文
-		String query = "SELECT * "
-			+ "FROM employee"
-			+ "WHERE id=?";
+		String query = "SELECT *"
+			+ " FROM employee"
+			+ " WHERE id=?";
 		
 		//検索実行
-		Map<String, Object>employee = jdbcTemplate.queryForMap(query,id);
+		Map<String, Object> employee = jdbcTemplate.queryForMap(query,id);
 		
 		return employee;
 	}
