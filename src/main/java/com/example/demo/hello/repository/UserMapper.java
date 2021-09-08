13b23026d1dffd26dea43aff2005db73ba769b62
maetaku <maetaku@localhost>
2021-09-08 11:40:52 +0900

    UserMapperインターフェース作成。

--- src/main/java/com/example/demo/hello/repository/UserMapper.java
+++ src/main/java/com/example/demo/hello/repository/UserMapper.java
@@ -1,5 +1,12 @@
 package com.example.demo.hello.repository;
 
-public interface UserMapper {
+import org.apache.ibatis.annotations.Mapper;
 
+import com.example.demo.hello.domain.user.model.MUser;
+
+@Mapper
+public interface UserMapper {
+	
+	/**ユーザー登録*/
+	public int insertOne(MUser user);
 }
