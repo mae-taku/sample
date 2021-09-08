aff634efb42caad7df11a4fa45255dd98fcaf56e
maetaku <maetaku@localhost>
2021-09-08 11:36:29 +0900

    ユーザーマスタのエンティティクラス？の作成。

--- src/main/java/com/example/demo/hello/domain/user/model/MUser.java
+++ src/main/java/com/example/demo/hello/domain/user/model/MUser.java
@@ -1,5 +1,17 @@
 package com.example.demo.hello.domain.user.model;
 
-public class MUser {
+import java.sql.Date;
 
-}
+import lombok.Data;
+
+@Data
+public class MUser {
+	private String userId;
+	private String password;
+	private String userName;
+	private Date birthday;
+	private Integer age;
+	private Integer gender;
+	private Integer departmentId;
+	private String role;
+	}
