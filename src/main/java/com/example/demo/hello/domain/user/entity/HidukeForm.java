fe873117516d038768af4b1be7e9eb561c308764
mae.taku <maetaku@localhost>
2021-10-05 23:57:24 +0900

    create entity class.

---
+++ src/main/java/com/example/demo/hello/domain/user/entity/HidukeForm.java
@@ -0,0 +1,14 @@
+package com.example.demo.hello.domain.user.entity;
+
+import lombok.Data;
+
+@Data
+public class HidukeForm {
+	private int id;
+	private	String hidukeId;
+	private String hidukeName;
+	private int	countYear;
+	private int countMonth;
+	private int	countDay;
+	
+}
