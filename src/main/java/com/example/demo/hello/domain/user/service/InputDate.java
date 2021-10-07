8934b1d53f527a73d60ef013b56008e2351efcc6
mae.taku <maetaku@localhost>
2021-10-08 01:20:56 +0900

    adjust date(Html) -> localDate(Java).[@DateTimeFormat]

--- src/main/java/com/example/demo/hello/domain/user/service/InputDate.java
+++ src/main/java/com/example/demo/hello/domain/user/service/InputDate.java
@@ -2,10 +2,16 @@ package com.example.demo.hello.domain.user.service;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class InputDate {
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate inputDate;
-
+	
+	
+	
 }
