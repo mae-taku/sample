bd264cdd9c533b2487fc4948280dd7bed8b3eddb
mae.taku <maetaku@localhost>
2021-10-07 13:57:07 +0900

    convert Entity-class.(HidukeForm -> HidukeOutForm)

--- src/main/java/com/example/demo/hello/DateCalcService.java
+++ src/main/java/com/example/demo/hello/DateCalcService.java
@@ -1,11 +1,14 @@
 package com.example.demo.hello;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.hello.domain.user.entity.HidukeForm;
+import com.example.demo.hello.domain.user.entity.HidukeOutForm;
 
 @Service
 public class DateCalcService {
@@ -15,9 +18,30 @@ public class DateCalcService {
 
 
 //	READ
-	public List<HidukeForm> selectAllDate(){
-//		System.out.println(selectAllDate());
-		return userMapper.selectAllDate();
+	public ArrayList<HidukeOutForm> selectAllDate(){
+		List<HidukeForm> list = userMapper.selectAllDate();//Mapperからのデータをリストに格納。
+//		LocalDate a = list.get(0).getLocalDate();
+//		System.out.println(a);
+//		HidukeForm b = list.get(0);//list内のHidukeFormをインスタンス化して取り出す
+//		System.out.println(b);	
+        ArrayList<HidukeOutForm> listForm = new ArrayList<HidukeOutForm>();//空のリストを作成
+
+        for (int i = 0; i < list.size(); i++) {
+            HidukeOutForm outForm = new HidukeOutForm();//リストに格納する出力データフォーマットをインスタンス化。
+            //以下で、mapperリストの各データを出力フォーマットへ入れ替え。HidukeForm -> HidukeOutForm
+    		outForm.setId(list.get(i).getId());
+    		outForm.setHidukeId(list.get(i).getHidukeId());
+    		outForm.setHidukeName(list.get(i).getHidukeName());
+    		outForm.setDate(list.get(i).calcDate());
+    		
+    		LocalDate sample = LocalDate.of(2021, 10, 7);//サンプルデータ ->入力値を入れるように変更予定
+    		LocalDate resultDate = sample.plusYears(list.get(i).getCountYear());
+    		outForm.setResultDate(resultDate);
+    		
+    		//出力データフォーマットをリストに格納。
+    		listForm.add(outForm);
+        }      
+		return listForm;
 	}
 	
 //	CREATE
