e5f05c88527a29a4b4a7bbfa4215bb32a3eb6e4a
mae.taku <maetaku@localhost>
2021-10-07 21:42:58 +0900

    add post-method with error(400). Validation failed for object='inputDate'. Error count: 1

--- src/main/java/com/example/demo/hello/Controller/DateCalcController.java
+++ src/main/java/com/example/demo/hello/Controller/DateCalcController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.hello.DateCalcService;
 import com.example.demo.hello.domain.user.entity.HidukeOutForm;
+import com.example.demo.hello.domain.user.service.InputDate;
 
 @Controller
 public class DateCalcController {
@@ -21,8 +24,14 @@ public class DateCalcController {
 //	
 	@Autowired
 	private DateCalcService service;
-	
-	@GetMapping("dateCalc/dateList")
+
+	@PostMapping("/dateCalc/dateList")	
+	public String postdate(@ModelAttribute("inputDate") InputDate inputDate, Model m) {
+		System.out.println(inputDate);
+		m.addAttribute("inputdate", inputDate);
+		return "dateCalc/dateList";
+	}
+	@GetMapping("/dateCalc/dateList")
 	public String top(Model m) {
 		List<HidukeOutForm>selectAll = service.selectAllDate();
 //		System.out.println(selectAll);
---
+++ src/main/java/com/example/demo/hello/domain/user/service/InputDate.java
@@ -0,0 +1,11 @@
+package com.example.demo.hello.domain.user.service;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class InputDate {
+	private LocalDate inputDate;
+
+}
